437f5c9a7b578c73e73159af5414daa543a92724
Ray Emory <remory@summitsemi.com>
2019-07-14 12:05:13 -0700

    Initial User class skeleton

---
+++ src/main/java/User.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+/** User class for storing user data.
+ *  Contains Username, Password, and other relevant information
+ */
+public class User {
+    private String username;
+    private String password;
+    private String hostname;
+
+    User(){
+        /** Base instantiation of User object.
+         *  String settings to come later.
+         */
+        username = null;
+        password = null;
+        hostname = null;
+    }
+
+    /**
+     * Creates a new <code>User</code> object.
+     *
+     * @param username
+     *        The username
+     *
+     * @param password
+     *        The User's password
+     *
+     * @param hostname
+     *        The server to which the user will connect
+     */
+    User(String username, String password, String hostname) {
+        this.username = username;
+        this.password = password;
+        this.hostname = hostname;
+    }
+
+
+}
