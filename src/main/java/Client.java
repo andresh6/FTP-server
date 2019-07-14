d26190afb51cfb9d771a126129f4d2986bca1131
dejef <dejef@pdx.edu>
2019-07-14 13:50:35 -0700

    implemented connect function

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,7 +1,51 @@
 import com.jcraft.jsch.*;
 
 public class Client {
-    public void run(){
+    JSch jsch;
+    ChannelSftp channelSftp; // the connection for the client
+    User user; // expand to data structure
 
+    Client() {
+        // create jsch
+        jsch = new JSch();
+        user = new User();
+
+        // connect ?
+        // pass in the user object
+        // instantiate the session
+    }
+
+    Client(User user) {
+        jsch = new JSch();
+        this.user = user;
+    }
+
+    Client(String username, String password, String hostname) {
+        jsch = new JSch();
+        user = new User(username, password, hostname);
+    }
+
+
+    public void connect(){
+        try {
+            // function that runs the code...
+            Session session = jsch.getSession(user.username, user.hostname, 22);
+
+            // make user object for username and password
+            session.setPassword(user.password);
+
+            session.connect();
+
+            Channel channel = session.openChannel("sftp");
+
+            channel.connect();
+
+            channelSftp = ((ChannelSftp) channel);
+
+        } catch (Exception e) {
+            System.err.println("You did something wrong.");
+            System.err.println(e);
+            System.exit(1);
+        }
     }
 }
