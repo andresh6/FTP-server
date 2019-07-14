7c893e703c3a40807ba72fb50585e5497616d93d
Ray Emory <remory@summitsemi.com>
2019-07-14 13:13:39 -0700

    added UserTest class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
-package com.company;
+import com.jcraft.jsch.*;
+
 
 public class Main {
 
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+
+public class UserTest {
+    private User user = new User();
+
+
+
+}
