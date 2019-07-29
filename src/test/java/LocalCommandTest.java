b6620f32b44ee6569dcbf8c512619a4ed869a441
dejef <dejef@pdx.edu>
2019-07-28 22:37:42 -0700

    Double checked that the ls stuff worked. Wrote part of one test but moved to Done... probably going to move it back.

---
+++ src/test/java/LocalCommandTest.java
@@ -0,0 +1,14 @@
+//import jdk.vm.ci.meta.Local;
+import org.junit.Test;
+
+public class LocalCommandTest {
+    @Test
+    public void testlsCommandTestDirectory() {
+        // File new create directory if one doesn't already exit
+        // add temp folder to show that paths work
+        // list everything at the project's root level
+        LocalCommand localCommand = new LocalCommand();
+        localCommand.ls(System.getProperty("user.dir") + "/testing");
+        // clean up temp dirs made
+    }
+}
