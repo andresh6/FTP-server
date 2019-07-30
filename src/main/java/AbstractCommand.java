10c56193bb50d6d9bf04596586f67f756dc1a10e
dejef <dejef@pdx.edu>
2019-07-30 09:44:44 -0700

    forgot currDir attribute

--- src/main/java/AbstractCommand.java
+++ src/main/java/AbstractCommand.java
@@ -2,6 +2,7 @@
  * command list
  */
 public abstract class AbstractCommand {
+    String currentWorkingDirectory;
     abstract void cd(String path);
     abstract void ls(String path);
     abstract void mkdir(String name);
