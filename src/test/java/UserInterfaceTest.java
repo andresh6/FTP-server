b295681ab91ae121184cc178026ab774eea3b5e3
andresh6 <43798349+andresh6@users.noreply.github.com>
2019-08-06 02:45:20 -0700

    Create UserInterfaceTest.java

---
+++ src/test/java/UserInterfaceTest.java
@@ -0,0 +1,60 @@
+import com.jcraft.jsch.JSch;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.*;
+
+public class UserInterfaceTest {
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    private User user = new User("agileteam6", "agileteam6", "34.83.11.14");
+    private Client client = new Client(user);
+    private UserInterface ui = new UserInterface();
+
+
+    @Test
+    public void testputfile() {
+        System.setOut(new PrintStream(outContent));
+        client.connect();
+        if (!client.channelSftp.isConnected()) {
+            fail("Could not connect to server");
+        }
+        client.put_file("/home/andres/Desktop/TestFolder/testing.txt", "/home/agileteam6");//perfect
+        client.listRemoteFiles();
+        assertThat(outContent.toString(), containsString("testing.txt"));
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testputmultiple() {
+        System.setOut(new PrintStream(outContent));
+        client.connect();
+        if (!client.channelSftp.isConnected()) {
+            fail("Could not connect to server");
+        }
+        ui.put_multiple(client, "mput /home/andres/Desktop/TestFolder /home/agileteam6 testing.txt testing2.txt testing3.txt");//perfect
+        client.listRemoteFiles();
+        assertThat(outContent.toString(), containsString("testing.txt"));
+        assertThat(outContent.toString(), containsString("testing2.txt"));
+        assertThat(outContent.toString(), containsString("testing3.txt"));
+
+        System.setOut(originalOut);
+    }
+    @Test
+    public void testgetfile(){
+        System.setOut(new PrintStream(outContent));
+        client.connect();
+        if (!client.channelSftp.isConnected()) {
+            fail("Could not connect to server");
+        }
+    }
+}
