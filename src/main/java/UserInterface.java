8bf5c40a962bc6c97978451f976e6f2de4c9d99a
andresh6 <43798349+andresh6@users.noreply.github.com>
2019-08-13 17:02:20 -0700

    Update UserInterface.java

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -331,7 +331,7 @@ public class UserInterface {
                     break;
                 case "lcd":
                     returnString = lcdHelp;
-                    break
+                    break;
                 case "pwd":
                     returnString = pwdHelp;
                     break;
